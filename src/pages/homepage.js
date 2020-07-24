0edb75e868e5480b78603facb7a1a880d0938750
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-07-24 12:39:11 +1000

    started working

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import background from '../assets/images/hpbackground.png';
 import Hamburger from '../components/hamburger/hamburger';
-import './homepage.css'
+import './pages.css'
 
 
 class Home extends Component {
   render (){
   
     return(
-        <div className="background">
+        <div className="homeBackground">
             <Hamburger/>
         </div>
     )
