106d28e9cc5e0342357c963965e5815f4f38bb15
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-07-24 16:13:00 +1000

    had to comment out the hamburger component to run heroku test

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -9,7 +9,7 @@ class Home extends Component {
     return(
         <div className="homepageBackground">
           <img src={backgroundImage}></img>
-          <Hamburger />
+          {/* <Hamburger /> */}
         </div>
     )
   }
