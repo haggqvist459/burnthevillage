2f84c79e6f81f0ed536e9e70c1a060e3e51006bd
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-07-30 20:43:17 +1000

    wrapped the components in app.js in themeprovider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core'
 import Home from './pages/homepage';
 import Upload from './pages/upload';
 import Terms from './pages/terms';
@@ -8,21 +9,24 @@ import Signup from './pages/signup';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Forgot from './pages/forgot';
+import theme from './theme';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Route exact path='/' component={Home}/>
-        <Route path='/upload' component={Upload}/>
-        <Route path='/terms' component={Terms}/>
-        <Route path='/signin' component={Signin}/>
-        <Route path='/signup' component={Signup}/>
-        <Route path='/contact' component={Contact}/>
-        <Route path='/about' component={About}/>
-        <Route path='/forgot' component={Forgot}/>
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path='/' component={Home} />
+          <Route path='/upload' component={Upload} />
+          <Route path='/terms' component={Terms} />
+          <Route path='/signin' component={Signin} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/about' component={About} />
+          <Route path='/forgot' component={Forgot} />
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
