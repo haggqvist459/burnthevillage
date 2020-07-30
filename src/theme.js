9d63914bdf41ecc2cacc4273ac2497ec7f41cc4f
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-07-30 17:56:40 +1000

    added fonts, primary & secondary color

---
+++ src/theme.js
@@ -0,0 +1,33 @@
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core';
+import './sass/fonts.scss'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#0976E3",
+        },
+        secondary: {
+            main: "#D4CE71"
+        }
+    },
+    typography: {
+
+        fontFamily: "ComicNeue",
+
+        h1: {
+            fontFamily: "Supercell-magic-webfont",
+        },
+        h2: {
+            fontFamily: "Supercell-magic-webfont",
+        },
+        h3: {
+            fontFamily: "Supercell-magic-webfont",
+        },
+        h4: {
+            fontFamily: "Supercell-magic-webfont",
+        },
+    },
+});
+theme = responsiveFontSizes(theme);
+
+export default theme;
