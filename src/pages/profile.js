d32edf862d33beda41c5844629f68ad5b3e9b24e
mahaluo <6709@ait.nsw.edu.au>
2020-07-31 00:04:01 +1000

    netlify test 5

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -6,7 +6,6 @@ import Footer from '../components/footer/footer';
 //import clashApi from 'clash-of-clans-api';
 import firebase from '../components/firebase/config';
 
-
 // const client = clashApi({
 //     token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQzZTUyMTUwLWJjODMtNDMwNi05ZWUxLWE0ZWQyZGJmZjU5OSIsImlhdCI6MTU5NTc3MjI0OCwic3ViIjoiZGV2ZWxvcGVyL2QwNDhlNjI1LTE0MjUtYzU2Yy01NTViLWY1MTIyMmZiNDAyZiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjYwLjI0MS4yMjkuMjM2Il0sInR5cGUiOiJjbGllbnQifV19.NCgihYBBP8e55BpViHcqR5U4ng2OJKTiZVyiPpOY0t_xVQxI-8PI1MH5KQvwpjZh9Y9NpHz-4tSOC0u9p_GjTA"
 // });
@@ -20,71 +19,71 @@ class Profile extends Component {
     }
 
 
-    componentDidMount() {
-        this.getData();
+    // componentDidMount() {
+    //     this.getData();
 
-    }
+    // }
 
-    getData() {
+    // getData() {
 
-        async function data() {
-            // try {
-            //     client
-            //     .token('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQzZTUyMTUwLWJjODMtNDMwNi05ZWUxLWE0ZWQyZGJmZjU5OSIsImlhdCI6MTU5NTc3MjI0OCwic3ViIjoiZGV2ZWxvcGVyL2QwNDhlNjI1LTE0MjUtYzU2Yy01NTViLWY1MTIyMmZiNDAyZiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjYwLjI0MS4yMjkuMjM2Il0sInR5cGUiOiJjbGllbnQifV19.NCgihYBBP8e55BpViHcqR5U4ng2OJKTiZVyiPpOY0t_xVQxI-8PI1MH5KQvwpjZh9Y9NpHz-4tSOC0u9p_GjTA')
+    //     // async function data() {
+    //     //     // try {
+    //     //     //     client
+    //     //     //     .token('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQzZTUyMTUwLWJjODMtNDMwNi05ZWUxLWE0ZWQyZGJmZjU5OSIsImlhdCI6MTU5NTc3MjI0OCwic3ViIjoiZGV2ZWxvcGVyL2QwNDhlNjI1LTE0MjUtYzU2Yy01NTViLWY1MTIyMmZiNDAyZiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjYwLjI0MS4yMjkuMjM2Il0sInR5cGUiOiJjbGllbnQifV19.NCgihYBBP8e55BpViHcqR5U4ng2OJKTiZVyiPpOY0t_xVQxI-8PI1MH5KQvwpjZh9Y9NpHz-4tSOC0u9p_GjTA')
 
-            //     .clanByTag('#92UY2UCR')
-            //     .then(response => console.log(response))
-            //     .catch(err => console.log(err));
+    //     //     //     .clanByTag('#92UY2UCR')
+    //     //     //     .then(response => console.log(response))
+    //     //     //     .catch(err => console.log(err));
 
-            // } catch (error) {
-            //     console.log(error);
-            // }
-            var proxyUrl = 'http://localhost:3000/',
-                targetUrl = 'https://api.clashofclans.com/v1/clans/%2392UY2UCR'
+    //     //     // } catch (error) {
+    //     //     //     console.log(error);
+    //     //     // }
+    //     //     // var proxyUrl = 'http://localhost:3000/',
+    //     //     //     targetUrl = 'https://api.clashofclans.com/v1/clans/%2392UY2UCR'
         
-            // const user = await fetch(proxyUrl + targetUrl, {
-            //     method: "GET",
-            //     headers: {
-            //         Authorization: 'Bearer ' + bearer
-            //     },
-            // });
+    //     //     // const user = await fetch(proxyUrl + targetUrl, {
+    //     //     //     method: "GET",
+    //     //     //     headers: {
+    //     //     //         Authorization: 'Bearer ' + bearer
+    //     //     //     },
+    //     //     // });
 
-            const clan = await fetch(proxyUrl + targetUrl, {
-                method: "GET",
-                Authorization: bearer
+    //     //     // const clan = await fetch(proxyUrl + targetUrl, {
+    //     //     //     method: "GET",
+    //     //     //     Authorization: bearer
                 
-            })
+    //     //     // })
 
-            const jsonClan = await clan.text();
-            if (jsonClan.error) {
-              throw jsonClan.error;
-            }
-            else {
-              console.log("user: " + jsonClan + " signed in.. ");
+    //     //     // const jsonClan = await clan.text();
+    //     //     // if (jsonClan.error) {
+    //     //     //   throw jsonClan.error;
+    //     //     // }
+    //     //     // else {
+    //     //     //   console.log("user: " + jsonClan + " signed in.. ");
               
-            }
+    //     //     // }
 
           
 
-            // const jsonUser = await user.json()
-            // .then(res => res.text())
-            // .then(text => console.log(text));
+    //     //     // const jsonUser = await user.json()
+    //     //     // .then(res => res.text())
+    //     //     // .then(text => console.log(text));
 
-            // if (jsonUser.error) {
-            //     throw jsonUser.error;
-            // }
-            // else {
-            //     //console.log("user: " + jsonUser + " signed in.. ");
-            // }
-        }
+    //     //     // if (jsonUser.error) {
+    //     //     //     throw jsonUser.error;
+    //     //     // }
+    //     //     // else {
+    //     //     //     //console.log("user: " + jsonUser + " signed in.. ");
+    //     //     // }
+    //     // }
 
 
-        try {
-            data();
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    //     // try {
+    //     //     data();
+    //     // } catch (error) {
+    //     //     console.log(error);
+    //     // }
+    // }
 
     signOut() {
         firebase.auth().signOut()
@@ -115,6 +114,8 @@ class Profile extends Component {
                                 <p className="profile_container__profile_row__profile_fields__upload_history">
                                     Upload history
                         </p>
+
+                        <a href="/.netlify/functions/profileData">Trigger Function</a>
                             </div>
 
                         </div>
