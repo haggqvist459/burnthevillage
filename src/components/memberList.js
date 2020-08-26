d1a1adc6b5a5bcc05c00bd4b99942eea749b6a34
Max Häggkvist <maxhaggkvist@192-168-1-103.tpgi.com.au>
2020-08-26 20:37:48 +1000

    fixed annoying warning

--- src/components/memberList.js
+++ src/components/memberList.js
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react';
 import { withRouter } from 'react-router';
 import '../sass/index.scss';
 import { Header, Footer, localConstants } from './';
-import { makeStyles, GridList, GridListTile, GridListTileBar, IconButton, Grid, Typography, CardMedia, CardActionArea } from '@material-ui/core';
-import { Info } from '@material-ui/icons';
+import { makeStyles, GridList, Grid, Typography, CardMedia, CardActionArea } from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
 import { clanActions } from '../store/actions';
 import { RandomProfileImage } from './utils/randomProfileImage'
@@ -58,9 +57,9 @@ const MemberList = ({ history }) => {
     function GetCol(member, index) {
 
         return (
-            <Grid container direction={"row"} justify={"space-between"} >
+            <Grid container direction={"row"} justify={"space-between"}>
 
-                <Grid container direction={"column"} item xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Grid container direction={"column"} justify={'center'} item xs={4} sm={4} md={4} lg={4} xl={4}>
                     <CardActionArea onClick={() => handleClick(member.tag)}>
                         <CardMedia
                             component="img"
@@ -90,12 +89,16 @@ const MemberList = ({ history }) => {
                     <Grid container direction={"row"} justify={"center"} style={{ marginBottom: '30px' }} item xs={12} sm={12} md={12} lg={12} xl={12}>
                         <Typography variant="h3">{clan && clan.name}</Typography>
                     </Grid>
-                    <GridList cellHeight={'auto'} style={{ height: '50vh', width: '80%' }} cols={2}>
-                        {memberList && memberList.map((member, index) => (
-                            <Grid key={index} container direction={"column"} justify={"space-between"} style={{ marginBottom: '10px' }} item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                {GetCol(member, index)}
-                            </Grid>
-                        ))}
+                    <GridList cellHeight={'auto'} style={{ height: '50vh', width: '80%' }}>
+                        <Grid container direction={'row'} item xs={12} sm={12} md={12} lg={12} xl={12}>
+                            {memberList && memberList.map((member, index) => {
+                                return (
+                                    <Grid key={index} container direction={"column"} justify={"space-between"} style={{ marginBottom: '10px' }} item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                        {GetCol(member, index)}
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
                     </GridList>
                 </Grid>
 
