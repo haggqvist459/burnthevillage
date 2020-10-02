4607971d0eb66f62e43b76b4aacb25331f80df29
Max Häggkvist <maxhaggkvist@Maxs-MacBook-Pro.local>
2020-10-02 13:23:45 +1000

    fnini

--- src/components/uploadHistory.js
+++ src/components/uploadHistory.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { Grid, Typography, CardActionArea, CardMedia, GridList, Button, Badge } from '@material-ui/core'
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Grid, Typography, CardActionArea, CardMedia, GridList, Button, Badge } from '@material-ui/core';
 import '../sass/index.scss';
 import { DynamicFeed } from '@material-ui/icons';
-function UploadHistory({ uploads }) {
-    console.log('props upload, ', uploads)
 
+function UploadHistory({ uploads }) {
+
+    const { uploadHistory } = useSelector(state => state.user);
     const [state, setState] = useState({
         uiState: {
             mounted: false,
@@ -19,7 +21,7 @@ function UploadHistory({ uploads }) {
     useEffect(() => {
 
         if (!state.uiState.mounted) {
-            if (uploads) {
+            if (uploadHistory) {
                 setState({
                     ...state,
                     uiState: {
@@ -27,7 +29,7 @@ function UploadHistory({ uploads }) {
                         mounted: true,
                     },
                     list: {
-                        content: uploads,
+                        content: uploadHistory,
                     }
                 })
                 console.log('list content: ', state.list.content);
@@ -47,7 +49,7 @@ function UploadHistory({ uploads }) {
                 }
             })
         }
-    }, [state, uploads])
+    }, [state, uploadHistory])
 
     function toggleMatches({ uploadItem }) {
         console.log('toggling to matches: ', uploadItem)
@@ -88,7 +90,7 @@ function UploadHistory({ uploads }) {
                 currentList: 'uploadHistory'
             },
             list: {
-                content: uploads
+                content: uploadHistory
             }
         })
     }
@@ -116,9 +118,10 @@ function UploadHistory({ uploads }) {
                     null
                 }
 
-                <Grid style={{ height: '120px' }}>
-                    <CardActionArea style={{ margin: '10px', height: '100%' }} onClick={() => toggleMatches({ uploadItem: item })}>
+                <Grid>
+                    <CardActionArea style={{ margin: '10px' }} onClick={() => toggleMatches({ uploadItem: item })}>
                         <CardMedia
+                            style={{ height: '140px' }}
                             component="img"
                             alt={"bucket base"}
                             image={item.imageUrl}
@@ -129,81 +132,97 @@ function UploadHistory({ uploads }) {
         )
     }
 
+    function ViewMatch() {
+        return (
+            <Grid container direction={'row'} justify={'space-around'}>
+                <Grid container direction={'row'} item xs={6} sm={6} md={6} lg={6} xl={6}>
+                    <CardActionArea style={{ marginRight: '10px' }}>
+                        <CardMedia
+                            style={{ maxHeight: '120px' }}
+                            component="img"
+                            alt={"bucket base"}
+                            image={state.list.content.imageUrl}
+                        />
+                    </CardActionArea>
+                </Grid>
+                <Grid container direction={'row'} item xs={6} sm={6} md={6} lg={6} xl={6}>
+                    <Grid container direction={'row'} item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Typography>matches: {state.list.content.matchPercentage}%</Typography>
+                    </Grid>
+                    <Typography>YouTube: link placeholder</Typography>
+                </Grid>
+            </Grid>
+        )
+    }
+
     return (
+
         <Grid>
-            {uploads !== null ?
+            {uploadHistory && uploadHistory ?
                 <Grid>
-                    {uploads && uploads ?
-                        <Grid>
-                            {state.uiState.currentList === 'uploadHistory' || state.uiState.currentList === 'uploadMatches' ?
-                                <GridList cellHeight={'auto'} style={{ height: '30vh', width: '100%', border: '1px', borderColor: 'black' }}>
+                    {state.uiState.currentList === 'uploadHistory' || state.uiState.currentList === 'uploadMatches' ?
+                        <GridList cellHeight={120} style={{ height: '50vh', width: '100%', border: '1px', borderColor: 'black' }}>
 
-                                    {state.uiState.currentList && state.uiState.currentList === "uploadHistory" ?
-                                        <Grid container direction={'row'} justify={'center'} item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                            {state.list.content && state.list.content.map((item, index) => {
-                                                return (
-                                                    <Grid key={index} container direction={"column"} justify={"space-between"} style={{ marginBottom: '10px', padding: '10px' }} item xs={5} sm={5} md={5} lg={5} xl={5}>
-                                                        {GetCell(item, index)}
-                                                    </Grid>
-                                                )
-                                            })}
-                                        </Grid>
-                                        :
-                                        null
-                                    }
-
-                                    {state.uiState.currentList && state.uiState.currentList === "uploadMatches" ?
-                                        <Grid container direction={'row'} justify={'center'} item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                            {state.list.content && state.list.content.map((item, index) => {
-                                                return (
-                                                    <Grid key={index} container direction={"column"} justify={"space-between"} style={{ marginBottom: '10px', padding: '10px' }} item xs={5} sm={5} md={5} lg={5} xl={5}>
-                                                        {GetCell(item, index)}
-                                                    </Grid>
-                                                )
-                                            })}
-                                        </Grid>
-                                        :
-                                        null
-                                    }
-
-                                </GridList>
+                            {state.uiState.currentList && state.uiState.currentList === "uploadHistory" ?
+                                <Grid container direction={'row'} justify={'center'} item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                    {state.list.content && state.list.content.map((item, index) => {
+                                        return (
+                                            <Grid key={index} container direction={"column"} justify={"space-between"} style={{ marginBottom: '10px', padding: '10px' }} item xs={5} sm={5} md={5} lg={5} xl={5}>
+                                                {GetCell(item, index)}
+                                            </Grid>
+                                        )
+                                    })}
+                                </Grid>
                                 :
                                 null
                             }
 
-                            {state.uiState.currentList === 'viewMatch' ?
-                            <Grid>
-
-
-
-
-                            </Grid>
-                            :
-                            null
+                            {state.uiState.currentList && state.uiState.currentList === "uploadMatches" ?
+                                <Grid container direction={'row'} justify={'center'} item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                    {state.list.content && state.list.content.map((item, index) => {
+                                        return (
+                                            <Grid key={index} container direction={"column"} justify={"space-between"} style={{ marginBottom: '10px', padding: '10px' }} item xs={5} sm={5} md={5} lg={5} xl={5}>
+                                                {GetCell(item, index)}
+                                            </Grid>
+                                        )
+                                    })}
+                                </Grid>
+                                :
+                                null
                             }
 
-                        </Grid>
+                        </GridList>
                         :
-                        <Grid container justify={'center'}>
-                            <Typography>no uploads</Typography>
-                        </Grid>
+                        null
                     }
-                    {state.uiState.currentList === "uploadMatches" ?
-                        <Grid container direction={'row'} justify={'center'} style={{ marginTop: '30px' }}>
-                            <Button style={{ textTransform: 'none', padding: '0' }} onClick={toggleHistory}>
-                                <Typography variant="h6">return to history</Typography>
-                            </Button>
+
+                    {state.uiState.currentList === 'viewMatch' ?
+                        <Grid>
+                            <ViewMatch />
+
                         </Grid>
                         :
                         null
                     }
+
                 </Grid>
                 :
                 <Grid container justify={'center'}>
-                    <Typography>loading...</Typography>
+                    <Typography>no uploads</Typography>
                 </Grid>
             }
+
+            {state.uiState.currentList === "uploadMatches" || state.uiState.currentList === "viewMatch" ?
+                <Grid container direction={'row'} justify={'center'} style={{ marginTop: '30px' }}>
+                    <Button style={{ textTransform: 'none', padding: '0' }} onClick={toggleHistory}>
+                        <Typography variant="h6">return to history</Typography>
+                    </Button>
+                </Grid>
+                :
+                null
+            }
         </Grid>
+
     )
 }
 
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -5,7 +5,7 @@ import '../sass/index.scss';
 import { Header, Footer, localConstants, firebase, UploadHistory } from '../components';
 import { CircularProgress, Grid, Typography, Button, Badge, CardActionArea, GridList, IconButton, makeStyles } from '@material-ui/core';
 import { clanActions, userActions } from '../store/actions';
-import { Group } from '@material-ui/icons';
+import { Group, GridOff } from '@material-ui/icons';
 import GetProfileImage from '../components/utils/getProfileImage';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,8 +17,8 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.background.paper,
     },
     gridList: {
-        width: '80vw',
-        height: '20vh',
+        width: '80%',
+        height: '10vh',
     },
     icon: {
         color: 'rgba(255, 255, 255, 0.54)',
@@ -72,19 +72,23 @@ const Profile = ({ history }) => {
                 dispatch(clanActions.getClan(localStorage.getItem(localConstants.CLAN_TAG)))
             })
         })
-       
+
     }, [dispatch]);
 
     function TagList() {
         return (
-            <Grid container direction={'row'} justify={'center'} style={{marginTop: '30px'}}>
-                <Typography variant={'h6'}>my tags</Typography>
+            <Grid container direction={'row'} style={{ marginTop: '10px' }}>
+
+                <Grid container direction={'row'} justify={'center'} item xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <Typography variant={'h6'}>tags</Typography>
+                </Grid>
 
                 <GridList cellHeight={10} className={classes.gridList} cols={1}>
                     {tagList.map((tag, index) => (
                         <Grid container direction={'row'} key={index}>
                             <Grid>
                                 <Typography>{tag}</Typography>
+
                             </Grid>
                         </Grid>
                     ))}
@@ -101,81 +105,67 @@ const Profile = ({ history }) => {
 
             <Grid className="content">
 
-                <Grid container direction={'row'} justify={'space-around'} item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <Grid container direction={'row'} justify={'space-around'} style={{ minHeight: '70vh' }} item xs={12} sm={12} md={12} lg={12} xl={12}>
 
                     {/* first row with profile picture and user information */}
                     <Grid container direction={'row'} justify={'space-around'} item xs={12} sm={12} md={5} lg={5} xl={5} style={{ marginTop: '5vh' }}>
-
-                        <Grid container direction={'row'} item xs={4} sm={4} md={4} lg={4} xl={4}>
-                            <CardActionArea onClick={signOut} style={{ textAlign: 'center' }}>
+                        <Grid container direction={'column'} item xs={5} sm={5} md={5} lg={5} xl={5} style={{ marginRight: '10px' }}>
+                            <CardActionArea onClick={signOut} style={{ textAlign: 'right' }}>
                                 <GetProfileImage number={5} />
                             </CardActionArea>
                         </Grid>
-
-                        <Grid container direction={'row'} justify={'space-around'} item xs={4} sm={4} md={4} lg={4} xl={4}>
-
-                            {localStorage.getItem(localConstants.DISPLAY_NAME) ?
-                                <Grid>
-                                    <Typography variant="h6">{localStorage.getItem(localConstants.DISPLAY_NAME)}</Typography>
-                                </Grid>
-                                :
-                                <Grid>
+                        <Grid container direction={'column'} item xs={5} sm={5} md={5} lg={5} xl={5}>
+                            <Grid style={{ marginBottom: '10px' }}>
+                                {localStorage.getItem(localConstants.DISPLAY_NAME) ?
+                                    <Typography variant="h6" style={{ textAlign: 'center' }}>{localStorage.getItem(localConstants.DISPLAY_NAME)}</Typography>
+                                    :
                                     <CircularProgress color="secondary" />
-                                </Grid>
-                            }
-
-                            {clan && clan.name ?
-                                <Grid>
-                                    <Button style={{ textTransform: 'none', padding: '0' }} onClick={handleClanClick}>
+                                }
+                            </Grid>
+                            <Grid style={{ marginBottom: '10px' }}>
+                                {tagList && tagList ?
+                                    <TagList />
+                                    :
+                                    <CircularProgress color="secondary" />
+                                }
+                            </Grid>
+                            <Grid style={{ marginBottom: '10px' }}>
+                            <Typography variant="h6" style={{ textAlign: 'center' }}>clan</Typography>
+                                {clan && clan.name ?
+                                    <Button style={{ textTransform: 'none', padding: '0', alignSelf: 'center' }} onClick={handleClanClick}>
                                         <Typography variant="h6">{clan.name}</Typography>
                                         <Badge badgeContent={clan.members} color="secondary">
-                                            <Group color="primary" style={{ paddingLeft: '10' }} />
+                                            <Group color="primary" />
                                         </Badge>
                                     </Button>
-
-                                </Grid>
-                                :
-                                <Grid>
-                                    {player && player.clan ?
-                                        <Grid>
+                                    :
+                                    <Grid>
+                                        {player && player.clan ?
                                             <Button style={{ textTransform: 'none', padding: '0' }} disabled>
                                                 <Typography variant="h6">{player.clan.name}</Typography>
+                                                <Badge badgeContent={0} color="secondary">
+                                                    <Group color="primary" style={{ paddingLeft: '10' }} />
+                                                </Badge>
                                             </Button>
-                                            <Badge badgeContent={0} color="secondary">
-                                                <Group color="primary" style={{ paddingLeft: '10' }} />
-                                            </Badge>
-                                        </Grid>
-                                        :
-                                        <Grid>
+                                            :
                                             <Button style={{ textTransform: 'none', padding: '0' }} disabled>
                                                 <Typography variant="h6">loading clan.. </Typography>
+                                                <Badge badgeContent={0} color="secondary">
+                                                    <Group color="primary" style={{ paddingLeft: '10' }} />
+                                                </Badge>
                                             </Button>
-                                            <Badge badgeContent={0} color="secondary">
-                                                <Group color="primary" style={{ paddingLeft: '10' }} />
-                                            </Badge>
-                                        </Grid>
-                                    }
-
-                                </Grid>
-                            }
-
+                                        }
+                                    </Grid>
+                                }
+                            </Grid>
                         </Grid>
-
-                        <Grid container direction={'row'} justify={'center'} item xs={10} sm={10} md={10} lg={10} xl={10}>
-                            {tagList && tagList ?
-                                <TagList />
-                                :
-                                <CircularProgress />
-                            }
-                        </Grid>
-
                     </Grid>
 
                     {/* second row with list of user upload history */}
-                    <Grid container direction={'row'} justify={'space-evenly'} item xs={12} sm={12} md={5} lg={5} xl={5} style={{ marginTop: '5vh' }}>
-                        <Typography variant={'h6'}>uploads</Typography>
-                        <Grid container direction={'column'} item xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <UploadHistory uploads={uploadHistory} />
+                    <Grid container direction={'row'} justify={'space-evenly'} item xs={12} sm={12} md={6} lg={6} xl={6} style={{ marginTop: '5vh' }}>
+                        <Typography variant={'h6'} style={{ marginBottom: '30px' }}>uploads</Typography>
+                        <Grid container direction={'column'} item xs={12} sm={12} md={12} lg={12} xl={12} style={{ height: '100%' }}>
+                            <UploadHistory />
                         </Grid>
                     </Grid>
                 </Grid>
